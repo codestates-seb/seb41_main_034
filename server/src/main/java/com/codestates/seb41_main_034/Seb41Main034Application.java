b114070e90bd3ec40dd0f0ca3df6c7a3ddcf8837
Jimin Kim <jmkim000000@gmail.com>
2023-01-05 18:57:10 +0900

    chore: Spring Boot 프로젝트 생성

---
+++ server/src/main/java/com/codestates/seb41_main_034/Seb41Main034Application.java
@@ -0,0 +1,13 @@
+package com.codestates.seb41_main_034;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb41Main034Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb41Main034Application.class, args);
+	}
+
+}
