4bc052ffe72fc8dcbb2132e3d19fbc5d88c6d774
kimdong-hun <kimdonghun426@gmail.com>
2023-01-10 00:22:04 +0900

    chore: 초기 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/globalStyle';
+import theme from './styles/theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <p>hello world!</p>
-    </>
+    </ThemeProvider>
   );
 }
 
