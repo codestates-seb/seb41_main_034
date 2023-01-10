9838a3dd8a4dde13f42c5362322d60d1982ee1fe
choi-eun-hwa <dms6778@gmail.com>
2023-01-10 14:25:54 +0900

    design: Login component 디자인 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import { GlobalStyle } from './styles/globalStyle';
-
+import LoginPage from './pages/LoginPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+        </Routes>
+      </BrowserRouter>
       <GlobalStyle />
-      <p>hello world!</p>
     </>
   );
 }
