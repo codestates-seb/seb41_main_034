19b9fce0603bf9907719ec81d00ef1487b1ccb21
kimdong-hun <kimdonghun426@gmail.com>
2023-01-11 02:47:59 +0900

    design: 회원가입 폼 디자인 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/globalStyle';
 import theme from './styles/theme';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Signup />
     </ThemeProvider>
   );
 }
