f04ccbebc7296726d4fae177440c480d7ac6dac4
kimdong-hun <kimdonghun426@gmail.com>
2023-01-11 03:28:06 +0900

    chore: 라우터 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components';
+import { Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './styles/globalStyle';
 import theme from './styles/theme';
 import Signup from './pages/Signup';
@@ -7,7 +8,9 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Signup />
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </ThemeProvider>
   );
 }
