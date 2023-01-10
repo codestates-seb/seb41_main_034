b082004e2ec734d0f77c23cbd30f0ae397e173de
KimEungChan <dmdcks000@naver.com>
2023-01-10 14:16:02 +0900

    design: Footer 디자인구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
+import Dfooter from './components/Footer/Desktop';
 import { GlobalStyle } from './styles/globalStyle';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <p>hello world!</p>
+      <Dfooter />
     </>
   );
 }
---
+++ client/src/components/Footer/Desktop/index.js
@@ -0,0 +1,18 @@
+import { Footer, FooterContainer, Text1, Text2 } from '../../../styles/footerStyle'
+
+const Dfooter = () => {
+    return (
+        <>
+            <FooterContainer>
+                <Footer>
+                <Text1>고객센터</Text1>
+                <Text1>3434-3434</Text1>
+                <Text1>평일 09:00 ~ 18:00 (주말 & 공휴일 제외)</Text1>
+                <Text2>상호면 푸드밋 이메일 kimdonghun426@gmail.com (제휴문의) 대표이사 쌈싸조</Text2>
+                </Footer>
+            </FooterContainer>
+        </>
+    )
+}
+
+export default Dfooter;
