3fc1cfe3e1f6ffce0499eacf488c57ecd4b92bed
choi-eun-hwa <dms6778@gmail.com>
2023-01-10 15:22:10 +0900

    rename: 파일이동

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/globalStyle';
-import LoginPage from './pages/LoginPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import theme from './styles/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />}></Route>
-        </Routes>
-      </BrowserRouter>
     </ThemeProvider>
   );
 }
