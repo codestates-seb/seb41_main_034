5f647c84937757aecc8d37c41fef916348370a93
Jimin Kim <jmkim000000@gmail.com>
2023-01-11 21:12:11 +0900

    fix: 나의 주문 조회 API 수정
    주문이 여러 번 중복되어 조회되는 현상 수정

--- server/src/main/java/com/codestates/seb41_main_034/order/repository/OrderRepository.java
+++ server/src/main/java/com/codestates/seb41_main_034/order/repository/OrderRepository.java
@@ -14,7 +14,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("select o.id from Order o where o.createdBy = ?1 and year(o.createdAt) = ?2")
     Page<Long> findIdByCreatedByAndYear(int createdBy, int year, Pageable pageable);
 
-    @Query("select o from Order o left join fetch o.orderProducts where o.id in ?1")
+    @Query("select distinct o from Order o left join fetch o.orderProducts where o.id in ?1")
     List<Order> findAllById(List<Long> ids, Sort sort);
 
 }
