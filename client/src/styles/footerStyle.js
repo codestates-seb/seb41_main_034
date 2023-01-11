1d97729ba3102f61f79d39e91350cadb18174956
KimEungChan <dmdcks000@naver.com>
2023-01-11 14:45:22 +0900

    design: Footer 디자인 구현

--- client/src/styles/footerStyle.js
+++ client/src/styles/footerStyle.js
@@ -1,35 +1,56 @@
 import styled from "styled-components";
 
-export const FooterContainer = styled.footer`
-    display: flex;
-    justify-content: center;
-    width:100%;
+export const FooterWrapper = styled.footer`
+    width: 100%;
     position: absolute;
     bottom: 0;
-    border-top: 1px solid black;
+    display: grid;
+    justify-items: center;
+    @media ${(props)=> props.theme.tablet}{
+        border-top: 1px solid ${(props)=> props.theme.grayColor};
+        padding: 0px;
+    }
+    @media ${(props)=> props.theme.mobile}{
+        padding: 0px;
+    }
 `
 
-export const Footer = styled.div`
-    //Desktop
-    width: 100%;
-    max-width: 1024px;
-    padding-top: 50px;
-    padding-bottom: 50px;
-    padding-left: 50px;
+export const FooterContainer = styled.footer`
     display: grid;
+    justify-items: center;
+    max-width: 1024px;
+    width: 100%;
+    border-top: 1px solid ${(props)=> props.theme.grayColor};
+    padding: 142px 0 40px 64px;
+    @media ${(props)=> props.theme.tablet}{
+        border: none;
+    }
+`
+
+export const FooterBlock = styled.div`
+    width: 100%;
+    display: grid;
+    @media ${(props)=> props.theme.tablet}{
+        width: 100%;
+        padding-left: 0px;
+    }
+    @media ${(props)=> props.theme.mobile}{
+        padding-left: 30px;
+    }
 `
 
 export const Text1 = styled.h5`
     //Desktop
-    font-size: 20px;
+    font-size: 15px;
     margin-bottom: 10px;
     //Tablet
-    @media screen and (max-width:768px){
-        font-size: 16px;
+    @media ${(props)=> props.theme.tablet}{
+        font-size: 13px;
     }
     //Mobile
-    @media screen and (max-width: 425px){
-       font-size: 12px;
+    @media ${(props)=> props.theme.mobile}{
+       font-size: 11px;
+       padding-right: 50px;
     }
 `
 
@@ -37,13 +58,14 @@ export const Text2 = styled.h6`
     //Desktop
     font-size: 12px;
     margin-top: 10px;
-    color: #ADB5BD;
+    color: ${(props)=>props.theme.grayColor};
     //Tablet
-    @media screen and (max-width:768px){
+    @media ${(props)=> props.theme.tablet}{
         font-size: 10px;
     }
     //Mobile
-    @media screen and (max-width: 425px){
+    @media ${(props)=> props.theme.mobile}{
        font-size: 8px;
+       padding-right: 50px;
     }
 `
