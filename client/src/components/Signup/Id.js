6f49240d74e03863c0ff027f13ad421867f29b32
kimdong-hun <kimdonghun426@gmail.com>
2023-01-12 15:19:34 +0900

    feat: 회원가입폼 기능 구현

---
+++ client/src/components/Signup/Id.js
@@ -0,0 +1,24 @@
+import {
+  SignupItem,
+  SignupLabel,
+  SignupInput,
+  ConfirmButton
+} from '../../styles/signupStyle';
+
+const Id = ({ id, setId }) => {
+  return (
+    <SignupItem>
+      <SignupLabel htmlFor="id">아이디</SignupLabel>
+      <SignupInput
+        type="text"
+        id="id"
+        aria-label="아이디를 입력하세요."
+        onChange={(e) => setId(e.target.value)}
+        value={id || ''}
+      />
+      <ConfirmButton>중복확인</ConfirmButton>
+    </SignupItem>
+  );
+};
+
+export default Id;
---
+++ client/src/components/Signup/Password.js
@@ -0,0 +1,36 @@
+import { SignupItem, SignupLabel, SignupInput } from '../../styles/signupStyle';
+
+const Password = ({
+  password,
+  passwordConfirm,
+  setPassword,
+  setPasswordConfirm
+}) => {
+  return (
+    <>
+      <SignupItem>
+        <SignupLabel htmlFor="password">비밀번호</SignupLabel>
+        <SignupInput
+          type="password"
+          id="password"
+          aria-label="비밀번호를 입력하세요."
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ''}
+        />
+      </SignupItem>
+
+      <SignupItem>
+        <SignupLabel htmlFor="passwordConfirm">비밀번호 확인</SignupLabel>
+        <SignupInput
+          type="password"
+          id="passwordConfirm"
+          aria-label="비밀번호를 한번 더 입력하세요."
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+          value={passwordConfirm || ''}
+        />
+      </SignupItem>
+    </>
+  );
+};
+
+export default Password;
