f2dad7c89209f64320508bd1b28d43154aaa9c43
choi-eun-hwa <dms6778@gmail.com>
2023-01-12 15:11:34 +0900

    design: 로그인폼 디자인 구현

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,38 +1,49 @@
 import {
   Header,
-  Loginbody,
-  LoginTextMain,
+  LoginContainer,
+  LoginHeader,
+  LoginTitle,
   LoginButton,
-  LoginInform,
-  SignupQuest,
-  SignupButton,
-  IdInput,
-  IdText,
-  PassWordInput,
-  PassWordText,
-  FindPassWordButton,
-  Footer
+  LoginList,
+  LoginItem,
+  ToSignupSpan,
+  ToSignup,
+  Input,
+  TextLabel,
+  FindPassWordButton
 } from '../styles/loginStyle';
+// import Footer from '../styles/footerStyle';
 
 function LoginForm() {
   return (
     <>
       <Header></Header>
-      <Loginbody>
-        <LoginTextMain>로그인</LoginTextMain>
-        <SignupQuest>
-          아직 계정이 없으신가요? <SignupButton>회원가입</SignupButton>
-        </SignupQuest>
-        <LoginInform>
-          <IdText>아이디</IdText>
-          <IdInput></IdInput>
-          <PassWordText>비밀번호</PassWordText>
-          <PassWordInput></PassWordInput>
-        </LoginInform>
+      <LoginContainer>
+        <LoginHeader>
+          <LoginTitle>로그인</LoginTitle>
+          <ToSignupSpan>
+            아직 계정이 없으신가요?{' '}
+            <ToSignup aria-label="회원가입 페이지로 이동">회원가입</ToSignup>
+          </ToSignupSpan>
+        </LoginHeader>
+        <LoginList>
+          <LoginItem>
+            <TextLabel htmlFor="id">아이디</TextLabel>
+            <Input type="text" id="id" aria-label="아이디를 입력하세요." />
+          </LoginItem>
+          <LoginItem>
+            <TextLabel htmlFor="id">비밀번호</TextLabel>
+            <Input
+              type="text"
+              id="password"
+              aria-label="비밀번호를 입력하세요."
+            />
+          </LoginItem>
+        </LoginList>
         <FindPassWordButton>비밀번호찾기</FindPassWordButton>
-        <LoginButton>로그인</LoginButton>
-      </Loginbody>
-      <Footer></Footer>
+        <LoginButton type="submit">로그인</LoginButton>
+      </LoginContainer>
+      {/* <Footer></Footer> */}
     </>
   );
 }
