88737d2ea4d52495cacce0f9d895e0b3e6e12eb2
choi-eun-hwa <dms6778@gmail.com>
2023-01-13 15:19:50 +0900

    design: 주문내역 디자인 구현

---
+++ client/src/components/OrderSummary.js
@@ -0,0 +1,17 @@
+import {
+  Header,
+  OrderContainer,
+  OrderContent
+} from '../styles/ordersummaryStyle';
+
+function OrderSummary() {
+  return (
+    <>
+      <Header></Header>
+      <OrderContainer>
+        <OrderContent></OrderContent>
+      </OrderContainer>
+    </>
+  );
+}
+export default OrderSummary;
