c2ba715619666a80e84f824d76bc9096b2f0900c
kimdong-hun <kimdonghun426@gmail.com>
2023-01-14 17:48:39 +0900

    feat: Footer 기능 구현

--- client/src/styles/footerStyle.js
+++ client/src/styles/footerStyle.js
@@ -1,71 +1,148 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const FooterWrapper = styled.footer`
-    width: 100%;
-    /* bottom: 0; */
-    /* position: fixed; */
-    display: grid;
-    justify-items: center;
-    @media ${(props)=> props.theme.tablet}{
-        border-top: 1px solid ${(props)=> props.theme.grayColor};
-        padding: 0px;
-    }
-    @media ${(props)=> props.theme.mobile}{
-        padding: 0px;
-    }
-`
+const FooterWrapper = styled.footer`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
 
-export const FooterContainer = styled.footer`
-    display: grid;
-    justify-items: center;
-    max-width: 1024px;
-    width: 100%;
-    border-top: 1px solid ${(props)=> props.theme.grayColor};
-    padding: 142px 0 40px 64px;
-    @media ${(props)=> props.theme.tablet}{
-        border: none;
-    }
-`
+const FooterContainer = styled.dl`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  width: 100%;
+  max-width: 1024px;
+  height: 320px;
+  padding: 0 0 40px 64px;
+  border-top: 1px solid ${(props) => props.theme.borderColor};
+`;
 
-export const FooterBlock = styled.div`
-    width: 100%;
-    display: grid;
-    @media ${(props)=> props.theme.tablet}{
-        width: 100%;
-        padding-left: 0px;
-    }
-    @media ${(props)=> props.theme.mobile}{
-        /* padding-left: 30px; */
-    }
-`
+const Info = styled.div``;
 
-export const Text1 = styled.h5`
-    //Desktop
-    font-size: 15px;
-    margin-bottom: 10px;
-    //Tablet
-    @media ${(props)=> props.theme.tablet}{
-        font-size: 13px;
-    }
-    //Mobile
-    @media ${(props)=> props.theme.mobile}{
-       font-size: 11px;
-       padding-right: 50px;
-    }
-`
+const InfoName = styled.dt`
+  font-size: 18px;
+  margin-bottom: 8px;
 
-export const Text2 = styled.h6`
-    //Desktop
+  @media ${(props) => props.theme.tablet} {
+    font-size: 16px;
+    margin-bottom: 4px;
+  }
+
+  @media ${(props) => props.theme.mobile} {
+    font-size: 14px;
+    margin-bottom: 4px;
+  }
+`;
+
+const InfoNumber = styled.dd`
+  margin-bottom: 16px;
+
+  @media ${(props) => props.theme.tablet} {
+    margin-bottom: 8px;
+  }
+
+  @media ${(props) => props.theme.mobile} {
+    margin-bottom: 8px;
+  }
+
+  a {
+    font-size: 24px;
+
+    @media ${(props) => props.theme.tablet} {
+      font-size: 22px;
+    }
+
+    @media ${(props) => props.theme.mobile} {
+      font-size: 20px;
+    }
+  }
+`;
+
+const InfoTime = styled.dd`
+  font-size: 16px;
+  margin-bottom: 16px;
+
+  @media ${(props) => props.theme.tablet} {
+    font-size: 14px;
+    margin-bottom: 8px;
+  }
+
+  @media ${(props) => props.theme.mobile} {
     font-size: 12px;
-    margin-top: 10px;
-    color: ${(props)=>props.theme.grayColor};
-    //Tablet
-    @media ${(props)=> props.theme.tablet}{
-        font-size: 10px;
+    margin-bottom: 8px;
+  }
+`;
+
+const EtcContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+
+  @media ${(props) => props.theme.tablet} {
+    gap: 4px;
+  }
+
+  @media ${(props) => props.theme.mobile} {
+    flex-direction: column;
+    gap: 4px;
+  }
+`;
+
+const Etc = styled.div`
+  display: flex;
+`;
+
+const EtcDt = styled.dt`
+  margin-right: 4px;
+  font-size: 12px;
+  color: ${(props) => props.theme.grayColor};
+
+  @media ${(props) => props.theme.tablet} {
+    margin-right: 2px;
+    font-size: 10px;
+  }
+
+  @media ${(props) => props.theme.mobile} {
+    margin-right: 2px;
+    font-size: 8px;
+  }
+`;
+
+const EtcDd = styled.dd`
+  font-size: 12px;
+  color: ${(props) => props.theme.grayColor};
+
+  @media ${(props) => props.theme.tablet} {
+    font-size: 10px;
+  }
+
+  @media ${(props) => props.theme.mobile} {
+    font-size: 8px;
+  }
+
+  a {
+    font-size: 12px;
+    color: ${(props) => props.theme.grayColor};
+
+    @media ${(props) => props.theme.tablet} {
+      font-size: 10px;
     }
-    //Mobile
-    @media ${(props)=> props.theme.mobile}{
-       font-size: 8px;
-       padding-right: 50px;
+
+    @media ${(props) => props.theme.mobile} {
+      font-size: 8px;
     }
-`
+  }
+`;
+
+export {
+  FooterWrapper,
+  FooterContainer,
+  Info,
+  InfoName,
+  InfoNumber,
+  InfoTime,
+  EtcContainer,
+  Etc,
+  EtcDt,
+  EtcDd
+};
