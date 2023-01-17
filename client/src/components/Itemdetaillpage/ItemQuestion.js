38bbf6ece6b3fdd701907bc011395391416c79eb
ChanEungKim <dmdcks000@naver.com>
2023-01-17 15:10:11 +0900

    design: 상품문의 디자인구현

--- client/src/components/Itemdetaillpage/ItemQuestion.js
+++ client/src/components/Itemdetaillpage/ItemQuestion.js
@@ -7,17 +7,19 @@ import {
 } from '../../styles/itemquestionlistStyle';
 
 const ItemQuestion = () => {
+  // {Question}
+  // 16:삼항연산자, 18,19,24:뿌려주기
   return (
     <>
       <FlexContainer2>
         <QAText>Question</QAText>
-        <ItemText>김응찬/바나나/2023. 01. 10</ItemText>
         <AnswerBox>답변완료</AnswerBox>
       </FlexContainer2>
+      <ItemText>김응찬/바나나/2023. 01. 10</ItemText>
       <AnswerText>해당 제품에 대한 배송이 너무 느려요.</AnswerText>
+      <QAText>Answer</QAText>
       <FlexContainer2>
-        <QAText>Answer</QAText>
-        <ItemText>푸드밋</ItemText>
+        <ItemText>from 푸드밋</ItemText>
       </FlexContainer2>
       <AnswerText>죄송합니다. 빠른 시일 내에 처리해드리겠습니다.</AnswerText>
     </>
