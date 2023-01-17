26a764d587cf94bc573a2e93d9a095f74f206f43
choi-eun-hwa <dms6778@gmail.com>
2023-01-17 15:28:14 +0900

    design: 후기 디자인 구현

---
+++ client/src/styles/ReviewStyle.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ReviewContainer = styled.div`
+  width: 520px;
+  height: 1205px;
+  margin: auto;
+  background-color: red;
+`;
+
+export const ReviewHeader = styled.div`
+  font-size: 50px;
+`;
+export const ReviewText = styled.div`
+  font-size: 20px;
+`;
+export const ReviewImage = styled.div`
+  width: 252px;
+  height: 210px;
+`;
+// export { ReviewContainer, ReviewHeader, ReviewText };
