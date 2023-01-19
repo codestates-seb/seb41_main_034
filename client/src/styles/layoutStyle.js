fcacc55e1430e3a518371767a6f7971e71a0cdd2
kimdong-hun <kimdonghun426@gmail.com>
2023-01-20 00:39:58 +0900

    design: padding 수정

--- client/src/styles/layoutStyle.js
+++ client/src/styles/layoutStyle.js
@@ -248,7 +248,7 @@ const FooterContainer = styled.dl`
   width: 100%;
   max-width: 1200px;
   height: 240px;
-  padding: 0 0 40px 64px;
+  padding: 0 0 64px 64px;
 
   @media ${(props) => props.theme.tablet} {
     padding-left: 32px;
