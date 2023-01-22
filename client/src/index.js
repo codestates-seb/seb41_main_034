6604a22543805b3e05dfd5acfe5f1c61acae453f
kimdong-hun <kimdonghun426@gmail.com>
2023-01-22 16:51:24 +0900

    feat: Link 상단 이동 구현

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store/store';
+import LinkToTop from './components/Layout/LinkToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
     <BrowserRouter>
+      <LinkToTop />
       <App />
     </BrowserRouter>
   </Provider>
