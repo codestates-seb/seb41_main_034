e82e1744c74dcfdc40b289fb5253aab7c911f33a
ChanEungKim <dmdcks000@naver.com>
2023-01-22 19:37:43 +0900

    test: 나의문의 API테스트 코드작성

--- client/src/components/Mypage/MyQuestion.js
+++ client/src/components/Mypage/MyQuestion.js
@@ -7,22 +7,39 @@ import {
   ItemLinkText
 } from '../../styles/myPageStyle';
 import { ReactComponent as CancelIcon } from '../../assets/icons/cancleIcon.svg';
+import { questionDeleteAPI, questionGetAPI } from '../../api/question';
+import { useEffect, useState } from 'react';
 
 const MyQuestion = () => {
+  const [question, setQuestion] = useState(null);
+
   const onRemove = () => {
     if (window.confirm('해당 상품에 대한 문의를 삭제하시겠습니까?')) {
+      const body = 'questionId';
+      questionDeleteAPI(body);
       alert('삭제되었습니다');
     } else {
       alert('취소했습니다.');
     }
   };
 
+  useEffect(() => {
+    const body = 'questionId';
+    const data = questionGetAPI(body);
+    setQuestion(data);
+    console.log(question);
+  }, ['question']);
+
   return (
     <ListHeader2>
       <LeftCotainer2>
-        <ItemLinkText>상품명</ItemLinkText>
+        <ItemLinkText aria-label="상품명에 대한 문의보기 버튼입니다.">
+          상품명
+        </ItemLinkText>
         <MarginSpace />
-        <ItemLinkText>상품에 대한 문의합니다..</ItemLinkText>
+        <ItemLinkText aria-label="상품명에 대한 문의보기 버튼입니다.">
+          상품에 대한 문의합니다..
+        </ItemLinkText>
       </LeftCotainer2>
       <RightContainer2>
         <ItemText>2023.1.13</ItemText>
