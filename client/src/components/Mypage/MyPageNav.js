e11186ee610427dadd8855d7cbf06d111481a014
ChanEungKim <dmdcks000@naver.com>
2023-01-22 19:28:54 +0900

    design: 마이페이지 Navbar label작성

--- client/src/components/Mypage/MyPageNav.js
+++ client/src/components/Mypage/MyPageNav.js
@@ -4,19 +4,19 @@ import { NavContainer, CatagoryBox } from '../../styles/myPageStyle';
 const MyPageNav = () => {
   return (
     <NavContainer>
-      <CatagoryBox to="">
+      <CatagoryBox aria-label="주소관리 카테고리버튼입니다." to="">
         주소관리
         <RightArrow />
       </CatagoryBox>
-      <CatagoryBox to="">
+      <CatagoryBox aria-label="주문목록 카테고리버튼입니다." to="">
         주문목록
         <RightArrow />
       </CatagoryBox>
-      <CatagoryBox to="">
+      <CatagoryBox aria-label="나의리뷰 카테고리버튼입니다." to="">
         나의리뷰
         <RightArrow />
       </CatagoryBox>
-      <CatagoryBox to="">
+      <CatagoryBox aria-label="나의문의 카테고리버튼입니다." to="">
         나의문의
         <RightArrow />
       </CatagoryBox>
