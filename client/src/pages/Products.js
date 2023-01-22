3488ff0c5bcafe5f5ad9d025774bb013e9a564fa
kimdong-hun <kimdonghun426@gmail.com>
2023-01-22 17:22:22 +0900

    design: 상품목록 디자인 수정

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -8,7 +8,10 @@ const Products = () => {
       <SortNavbar />
 
       <ProductList>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((el, idx) => (
+        {[
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24
+        ].map((el, idx) => (
           <ProductItem key={idx} />
         ))}
       </ProductList>
