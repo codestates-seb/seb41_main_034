52f21db49904640b21a22b305fc9676c58437cbd
choi-eun-hwa <dms6778@gmail.com>
2023-01-26 15:20:16 +0900

    feat: 주소 기능구현

---
+++ client/src/api/address.js
@@ -0,0 +1,15 @@
+import { authAPI } from './customAxios';
+
+//기본 주소 조회
+const userAddressGetAPI = async (userAddressId) => {
+  try {
+    // await authAPI.get(`/user-address/${userAddressId}/`);
+    const address = await authAPI.get(`/user-address`);
+    console.log('address', address);
+    return address.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { userAddressGetAPI };
